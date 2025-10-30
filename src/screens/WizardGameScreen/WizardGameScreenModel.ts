3a25f32c5e3a5716525e07f6dd3ab76cbb601a50
UCSDreisbach <cdreisbach@ucsd.edu>
2025-10-29 18:20:46 -0700

    added a frame for the hieght and width of the wizard Game

--- src/screens/WizardGameScreen/WizardGameScreenModel.ts
+++ src/screens/WizardGameScreen/WizardGameScreenModel.ts
@@ -1,3 +1,14 @@
-export class WizardGameScreenModel {
+import { STAGE_WIDTH, STAGE_HEIGHT } from "../../constants";
 
+export class WizardGameScreenModel {
+    public width: number;
+    public height: number;
+
+    /**
+     * constructs the game model window
+     */
+    constructor() {
+        this.width = STAGE_WIDTH;
+        this.height = STAGE_HEIGHT;
+    }
 }
