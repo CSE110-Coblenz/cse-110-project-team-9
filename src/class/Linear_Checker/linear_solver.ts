531fbb26351e450d08b7ff4910e104168a2ff1db
Meenakshi Manikandan <2mmanikan@gmail.com>
2025-11-08 15:20:02 -0800

    added types to function arguments

--- src/class/Linear_Checker/linear_solver.ts
+++ src/class/Linear_Checker/linear_solver.ts
@@ -1,7 +1,7 @@
 
 //Function Format: y=mx+b
 //solve for y, m, x, or b given the other 3 (what to solve for depends on problem type. If problem_type is m, we solve for slope)
-export function linear_equation_solver(val1, val2, val3, problem_type, user_input){
+export function linear_equation_solver(val1: Number, val2: Number, val3: Number, problem_type: Number, user_input: Number){
 	if (problem_type == "m"){
 		// m = (y-b)/x   ----> val1 = y, val2 = b, val3 = x
 		if (user_input == (val1-val2)/val3){
