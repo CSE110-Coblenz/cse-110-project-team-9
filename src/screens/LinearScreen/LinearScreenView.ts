c7a4d3725cafc46273a4f4caace4961310e59fd7
Meenakshi Manikandan <2mmanikan@gmail.com>
2025-11-15 00:59:44 -0800

    Added Linear Graph Generation Capability. Works for Positive Slopes so far. The slope and y-intercept are integers, and for now are randomly generated from the range 0 to 5.

--- src/screens/LinearScreen/LinearScreenView.ts
+++ src/screens/LinearScreen/LinearScreenView.ts
@@ -14,17 +14,72 @@ export class LinearScreenView implements View{
 	  const group = new Konva.Group({visible: false});
 	  
 
+	  
+
+	  //----------------Creating Graph-----------------------------------------------
+
+          
+          let m = Math.floor((Math.random() * (5 - (0))) + 0 )   ;  //maximum possible slope is 5, minimum is 0. Will add functionality for negative slopes.
+	  let b = Math.floor((Math.random() * (5 - (0))) + 0 )   ; 
+	  let width = STAGE_WIDTH/3;
+	  let height = STAGE_HEIGHT/3 + 30;
+	  let ax_len = (200); 
+	  let num_ticks = 10;
+	  let second_point_x = ( (num_ticks - b) / m )
+        
+
+
 
           //Create Line
           const line = new Konva.Line({
-	  points: [0,0,50,50,100,100],  //placeholder line, this will change. Is represented as [x1,y1,x2,y2...]
-	  stroke: 'black'
-
+	  	points: [width , height-(b * (ax_len / num_ticks)), width + (second_point_x * (ax_len / num_ticks)) , height - ( (m*second_point_x+b) * (ax_len / num_ticks) )],  //placeholder line, this will change. Is represented as [x1,y1,x2,y2...]
+	  	//Points on linear graph are (0,y_intercept) and (second_point_x, m(second_point_x)+b)
+		//
+	  	stroke: 'red',
+		strokeWidth: 3
           });
 
 
-          //add line to group
+	  //Create Axes
+	  const x_axis = new Konva.Line({
+		points: [width, height, width + ax_len, height],
+		stroke: 'black',
+		strokeWidth: 3
+	  });
+
+
+	  const y_axis = new Konva.Line({
+		points: [width, height, width, height - ax_len],
+		stroke: 'black',
+		strokeWidth: 3
+	  });
+
           group.add(line);
+          group.add(x_axis);
+	  group.add(y_axis);
+
+	  //Axis Label X
+          for (let i=1; i<=10; i++){
+		group.add(new Konva.Line({
+			points: [width + i * (ax_len / num_ticks), height, width + i * (ax_len / num_ticks), height - ax_len],
+			stroke: 'black',
+			strokeWidth: 1
+		}));
+	  }
+
+	  //Axis Label Y
+          for (let i=1; i<=10; i++){
+		group.add(new Konva.Line({
+			points: [width, height - i * (ax_len / num_ticks)   , width + ax_len ,  height - i * (ax_len / num_ticks)      ],
+			stroke: 'black',
+			strokeWidth: 1
+		}));
+	  }
+
+	  //-----------------------------------------------------------------------------
+
+
+
 
 
 	  //add to instance definition
