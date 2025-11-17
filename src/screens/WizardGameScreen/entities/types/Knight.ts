7d09045bc087feb43ec184b52a32aceda3d3b150
Luna <cdreisbach@ucsd.edu>
2025-11-16 19:35:02 -0800

    merged 2 todos

--- src/screens/WizardGameScreen/entities/types/Knight.ts
+++ src/screens/WizardGameScreen/entities/types/Knight.ts
@@ -68,8 +68,7 @@ export const KNIGHT_ANIMATIONS: Record<KnightAnimation, number[]> = {
 
 export type KnightAudioMap = typeof KNIGHT_AUDIO;
 
-//TODO: grab correct foramt .100 seconds per frame for any given animation
-//TODO: convert to mp4 instead mp3 does not do milliseconds
+//TODO: grab correct foramt .100 seconds per frame for any given animation mp4
 export const KNIGHT_AUDIO = {
     walk: "/wizardminigame/audio/8-bit-grass-footsteps-2-408574.mp3",
     attackbow: "/wizardminigame/audio/bow_release-85040.mp3",
